0bb9ff72027f905876b7f1a1972d739439e5519c
ibsadiq <33308208+ibsadiq@users.noreply.github.com>
2023-07-30 13:59:15 +0100

    static

--- call2fix/settings/base.py
+++ call2fix/settings/base.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_FINDERS = [
-    # "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
